d845c83741d4bfc761d9b8d446dc3a8672d58c99
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-22 16:54:13 -0600

    saving progress

--- R/week5.R
+++ R/week5.R
@@ -7,3 +7,8 @@ Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_nam
 Anotes_tbl <- read_csv(file = "../data/Anotes.csv")
 Bdata_tbl <- read_tsv(file = "../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10)))
 Bnotes_tbl <- read_tsv(file = "../data/Bnotes.txt")
+
+# Data Cleaning
+Aclean_tbl <- Adata_tbl |> 
+  separate(col = qs, into = paste0("q", 1:5)) |>
+  mutate(datadate = mdy_hms(datadate)) 
