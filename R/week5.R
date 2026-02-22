09f5f0e66b40bc47497050f478b8883d056fff92
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-22 16:48:45 -0600

    Looks like I got it in correctly

--- R/week5.R
+++ R/week5.R
@@ -5,5 +5,5 @@ library(tidyverse)
 # Data Import
 Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 Anotes_tbl <- read_csv(file = "../data/Anotes.csv")
-Bdata_tbl <- read_delim(file = "../data/Bparticipants.dat", delim = " ", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
-
+Bdata_tbl <- read_tsv(file = "../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10)))
+Bnotes_tbl <- read_tsv(file = "../data/Bnotes.txt")
