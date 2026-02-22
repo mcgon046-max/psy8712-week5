f9bbee96c711d548cc6eb0127fd119311105e98c
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-22 16:42:01 -0600

    Lab A's data and notes look goo, trying to fix B data

--- R/week5.R
+++ R/week5.R
@@ -1,3 +1,9 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+
+# Data Import
+Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+Anotes_tbl <- read_csv(file = "../data/Anotes.csv")
+Bdata_tbl <- read_delim(file = "../data/Bparticipants.dat", delim = " ", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+
